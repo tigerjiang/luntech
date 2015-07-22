27a3e7a3db431b4b8355eb33c326f4fb1473a72c
Tiger Jiang <tiger.jiang86@gmail.com>
2015-07-22 22:07:39 +0800

    Launcher : Parser the xml input stream [part 1]

--- LuntechLauncher/src/com/luntech/launcher/CustomApplication.java
+++ LuntechLauncher/src/com/luntech/launcher/CustomApplication.java
@@ -6,17 +6,16 @@ import java.util.ArrayList;
 public class CustomApplication {
     public Group mGroup;
     public ArrayList<Module> mModules;
-    public ArrayList<App> mApps;
 
     static class Group {
         public static final String GROUP_TAG = "group";
         public static final String GROUP_CODE_TAG = "code";
         public static final String GROUP_MOVEABLE_TAG = "moveable";
         public static final String GROUP_GROUP_FLAG_TAG = "group_flag";
-        public static final String GROUP_NAME_TAG = "name";
-        public static final String GROUP_SHOWTEXT_TAG = "showtext";
-        public static final String GROUP_BG_TAG = "bg";
-        public static final String GROUP_ICON_TAG = "icon";
+        public static final String GROUP_NAME_TAG = "group_name";
+        public static final String GROUP_SHOWTEXT_TAG = "group_showtext";
+        public static final String GROUP_BG_TAG = "group_bg";
+        public static final String GROUP_ICON_TAG = "group_icon";
 
         public Group() {
 
@@ -30,6 +29,13 @@ public class CustomApplication {
         public String groupBg;
         public String groupIcon;
 
+        @Override
+        public String toString() {
+            return "Group [groupCode=" + groupCode + ", groupMoveable=" + groupMoveable
+                    + ", groupFlag=" + groupFlag + ", groupName=" + groupName + ", groupShowText="
+                    + groupShowText + ", groupBg=" + groupBg + ", groupIcon=" + groupIcon + "]";
+        }
+
     }
 
     static class Module {
@@ -37,10 +43,11 @@ public class CustomApplication {
         public static final String MODULE_CODE_TAG = "code";
         public static final String MODULE_REPLACE_TAG = "replace";
         public static final String MODULE_TYPE_TAG = "type";
-        public static final String MODULE_SHOWTEXT_TAG = "showtext";
-        public static final String MODULE_BG_TAG = "bg";
-        public static final String MODULE_ICON_TAG = "icon";
-        public static final String MODULE_SHADOW_TAG = "shadow";
+        public static final String MODULE_SHOWTEXT_TAG = "module_showtext";
+        public static final String MODULE_BG_TAG = "module_bg";
+        public static final String MODULE_ICON_TAG = "module_icon";
+        public static final String MODULE_SHADOW_TAG = "module_shadow";
+        public ArrayList<App> mApps;
 
         public Module() {
 
@@ -53,18 +60,35 @@ public class CustomApplication {
         public String moduleBg;
         public String moduleIcon;
         public String moduleShadow;
+
+        public void addApp(App app) {
+            if (mApps == null) {
+                mApps = new ArrayList<CustomApplication.App>();
+            } else {
+                mApps.add(app);
+            }
+        }
+
+        @Override
+        public String toString() {
+            return "Module [mApps=" + mApps + ", moduleCode=" + moduleCode + ", moduleReplace="
+                    + moduleReplace + ", moduleType=" + moduleType + ", moduleShowText="
+                    + moduleShowText + ", moduleBg=" + moduleBg + ", moduleIcon=" + moduleIcon
+                    + ", moduleShadow=" + moduleShadow + "]";
+        }
+
     }
 
     static class App {
         public static final String APP_TAG = "app";
         public static final String APPS_TAG = "apps";
-        public static final String APP_NAME_TAG = "name";
-        public static final String APP_PACKAGE_NAME_TAG = "package_name";
-        public static final String APP_ACTIVITY_TAG = "activity";
-        public static final String APP_VERSION_TAG = "version";
-        public static final String APP_SIZE_TAG = "size";
-        public static final String APP_ICON_TAG = "icon";
-        public static final String APP_URL_TAG = "url";
+        public static final String APP_NAME_TAG = "app_name";
+        public static final String APP_PACKAGE_NAME_TAG = "app_package_name";
+        public static final String APP_ACTIVITY_TAG = "app_activity";
+        public static final String APP_VERSION_TAG = "app_version";
+        public static final String APP_SIZE_TAG = "app_size";
+        public static final String APP_ICON_TAG = "app_icon";
+        public static final String APP_URL_TAG = "app_url";
 
         public App() {
 
@@ -77,6 +101,14 @@ public class CustomApplication {
         public int appSize;
         public String appIcon;
         public String appUrl;
+
+        @Override
+        public String toString() {
+            return "App [appName=" + appName + ", appPackagename=" + appPackagename
+                    + ", appActivity=" + appActivity + ", appVersion=" + appVersion + ", appSize="
+                    + appSize + ", appIcon=" + appIcon + ", appUrl=" + appUrl + "]";
+        }
+
     }
 
     public void addModule(Module module) {
@@ -87,11 +119,4 @@ public class CustomApplication {
         }
     }
 
-    public void addApp(App app) {
-        if (mApps == null) {
-            mApps = new ArrayList<CustomApplication.App>();
-        } else {
-            mApps.add(app);
-        }
-    }
 }
--- LuntechLauncher/src/com/luntech/launcher/ToolUtils.java
+++ LuntechLauncher/src/com/luntech/launcher/ToolUtils.java
@@ -10,9 +10,12 @@ import android.util.Log;
 
 import com.luntech.launcher.secondary.ApplicationInfo;
 
+import org.xmlpull.v1.XmlPullParser;
 import org.xmlpull.v1.XmlPullParserException;
+import org.xmlpull.v1.XmlPullParserFactory;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -46,6 +49,58 @@ public class ToolUtils {
     }
 
     public static ArrayList<AppItem> getInfoFromConfig(Context context, int fileId) {
+        Resources r = context.getResources();
+        XmlResourceParser xrp = r.getXml(fileId);
+        ArrayList<AppItem> applist = new ArrayList<AppItem>();
+        AppItem app = null;
+        try {
+            while (xrp.getEventType() != XmlResourceParser.END_DOCUMENT) {
+                if (xrp.getEventType() == XmlResourceParser.START_TAG) {
+                    String name = xrp.getName();
+                    Log.d(TAG, name);
+
+                    if (name.equals(APP_TAG)) {
+                        app = new AppItem(context);
+                        app.setName(xrp.getAttributeValue(0));
+                    } else if (name.equals(TYPE_TAG)) {
+                        app.setType(xrp.nextText());
+                    } else if (name.equals(ACTIVITY_TAG)) {
+                        app.setActivityName(xrp.nextText());
+                    } else if (name.equals(LABEL_TAG)) {
+                        app.setLabel(xrp.nextText());
+                    } else if (name.equals(BACKGROUND_TAG)) {
+                        app.setBackground(xrp.nextText());
+                    } else if (name.equals(SHADOW_TAG)) {
+                        app.setShadow(xrp.nextText());
+                    } else if (name.equals(LOGO_TAG)) {
+                        app.setLogo(xrp.nextText());
+                    } else if (name.equals(CONFIGURED_TAG)) {
+                        app.setConfigured(xrp.nextText());
+                    } else if (name.equals(AVAILABLE_TAG)) {
+                        app.setAvailable(xrp.nextText());
+                    } else if (name.equals(CONFIGURED_ACTIVITY_TAG)) {
+                        app.setConfiguredComponent(xrp.nextText());
+                    }
+                } else if (xrp.getEventType() == XmlResourceParser.END_TAG) {
+                    String name = xrp.getName();
+                    Log.d(TAG, name);
+                    if (name.equals(APP_TAG)) {
+                        Log.d(TAG, "app " + app.toString());
+                        applist.add(app);
+                    }
+                }
+                xrp.next();
+            }
+
+        } catch (XmlPullParserException e) {
+            Log.e(TAG, "XmlPullParserException occurs " + e);
+        } catch (IOException e) {
+            Log.e(TAG, "packagefilter occurs " + e);
+        }
+        return applist;
+    }
+
+    public static ArrayList<CustomApplication> getCustomInfoFromConfig(Context context, int fileId) {
         Resources r = context.getResources();
         XmlResourceParser xrp = r.getXml(fileId);
         ArrayList<AppItem> applist = new ArrayList<AppItem>();
@@ -93,44 +148,135 @@ public class ToolUtils {
         } catch (IOException e) {
             Log.e(TAG, "packagefilter occurs " + e);
         }
-        return applist;
+        return null;
+    }
+
+    public static ArrayList<CustomApplication> getCustomInfoFromConfig(Context context,
+            InputStream is) {
+        ArrayList<CustomApplication> applications = new ArrayList<CustomApplication>();
+        CustomApplication application = null;
+        CustomApplication.Module module = null;
+        ArrayList<CustomApplication.App> apps = null;
+        CustomApplication.App app = null;
+        try {
+            XmlPullParserFactory factory = XmlPullParserFactory.newInstance();
+            XmlPullParser parser = factory.newPullParser();
+            parser.setInput(is, "utf-8");
+            while (parser.getEventType() != XmlResourceParser.END_DOCUMENT) {
+                if (parser.getEventType() == XmlResourceParser.START_TAG) {
+                    String name = parser.getName();
+                    Log.d(TAG, name);
+                    // group info
+                    if (name.equals(CustomApplication.Group.GROUP_TAG)) {
+                        application = new CustomApplication();
+                        application.mGroup = new CustomApplication.Group();
+                        application.mGroup.groupCode = parser.getAttributeValue(0);
+                        application.mGroup.groupMoveable = Integer.parseInt(parser
+                                .getAttributeValue(1));
+                        application.mGroup.groupFlag = Integer
+                                .parseInt(parser.getAttributeValue(2));
+                    } else if (name.equals(CustomApplication.Group.GROUP_NAME_TAG)) {
+                        application.mGroup.groupName = parser.nextText();
+                    } else if (name.equals(CustomApplication.Group.GROUP_SHOWTEXT_TAG)) {
+                        application.mGroup.groupShowText = parser.nextText();
+                    } else if (name.equals(CustomApplication.Group.GROUP_BG_TAG)) {
+                        application.mGroup.groupBg = parser.nextText();
+                    } else if (name.equals(CustomApplication.Group.GROUP_ICON_TAG)) {
+                        application.mGroup.groupIcon = parser.nextText();
+                    }
+                    // Module info
+
+                    else if (name.equals(CustomApplication.Module.MODULE_TAG)) {
+                        module = new CustomApplication.Module();
+                        module.moduleCode = parser.getAttributeValue(0);
+                        module.moduleReplace = Integer.parseInt(parser.getAttributeValue(1));
+                        module.moduleType = Integer.parseInt(parser.getAttributeValue(2));
+                    } else if (name.equals(CustomApplication.Module.MODULE_SHOWTEXT_TAG)) {
+                        module.moduleShowText = parser.nextText();
+                    } else if (name.equals(CustomApplication.Module.MODULE_BG_TAG)) {
+                        module.moduleBg = parser.nextText();
+                    } else if (name.equals(CustomApplication.Module.MODULE_ICON_TAG)) {
+                        module.moduleIcon = parser.nextText();
+                    } else if (name.equals(CustomApplication.Module.MODULE_SHADOW_TAG)) {
+                        module.moduleShadow = parser.nextText();
+                        // apps
+                    } else if (name.equals(CustomApplication.App.APPS_TAG)) {
+                        apps = new ArrayList<CustomApplication.App>();
+                    } else if (name.equals(CustomApplication.App.APP_TAG)) {
+                        app = new CustomApplication.App();
+                    } else if (name.equals(CustomApplication.App.APP_NAME_TAG)) {
+                        app.appName = parser.nextText();
+                    } else if (name.equals(CustomApplication.App.APP_PACKAGE_NAME_TAG)) {
+                        app.appPackagename = parser.nextText();
+                    } else if (name.equals(CustomApplication.App.APP_ACTIVITY_TAG)) {
+                        app.appActivity = parser.nextText();
+                    } else if (name.equals(CustomApplication.App.APP_VERSION_TAG)) {
+                        app.appVersion = Integer.parseInt(parser.nextText());
+                    } else if (name.equals(CustomApplication.App.APP_SIZE_TAG)) {
+                        app.appSize = Integer.parseInt(parser.nextText());
+                    } else if (name.equals(CustomApplication.App.APP_ICON_TAG)) {
+                        app.appIcon = parser.nextText();
+                    } else if (name.equals(CustomApplication.App.APP_URL_TAG)) {
+                        app.appUrl = parser.nextText();
+                    }
+
+                } else if (parser.getEventType() == XmlResourceParser.END_TAG) {
+                    String name = parser.getName();
+                    Log.d(TAG, name);
+                    if (name.equals(CustomApplication.App.APP_TAG)) {
+                        Log.d(TAG, "end app " + app.toString());
+                        apps.add(app);
+                    } else if (name.equals(CustomApplication.App.APPS_TAG)) {
+                        module.mApps = apps;
+                        Log.d(TAG, "end apps " + module.mApps.toString());
+                    } else if (name.equals(CustomApplication.Module.MODULE_TAG)) {
+                        application.addModule(module);
+                        Logger.d("end module" + module.toString());
+                    } else if (name.equals(CustomApplication.Group.GROUP_TAG)) {
+                        applications.add(application);
+                        Logger.d("end group" + application.toString());
+                    }
+                }
+                parser.next();
+            }
+
+        } catch (XmlPullParserException e) {
+            Log.e(TAG, "XmlPullParserException occurs " + e);
+        } catch (IOException e) {
+            Log.e(TAG, "packagefilter occurs " + e);
+        }
+        return applications;
     }
 
     public void setConfigured(Context context, ApplicationInfo app, boolean configured) {
-        SharedPreferences sp = context.getSharedPreferences(CUSTOM_INFO,
-                Context.MODE_PRIVATE);
+        SharedPreferences sp = context.getSharedPreferences(CUSTOM_INFO, Context.MODE_PRIVATE);
         Editor spe = sp.edit();
         spe.putBoolean(app.mTitle.toString(), configured);
         spe.commit();
     }
 
     public void setConfiguredPkg(Context context, int index, String pkg) {
-        SharedPreferences sp = context.getSharedPreferences(CUSTOM_INFO,
-                Context.MODE_PRIVATE);
+        SharedPreferences sp = context.getSharedPreferences(CUSTOM_INFO, Context.MODE_PRIVATE);
         Editor spe = sp.edit();
-        spe.putString("category"+"_"+index , pkg);
+        spe.putString("category" + "_" + index, pkg);
         spe.commit();
     }
 
     public String getConfiguredPkg(Context context, String name) {
-        SharedPreferences sp = context.getSharedPreferences(CUSTOM_INFO,
-                Context.MODE_PRIVATE);
-        return sp.getString(name , null);
+        SharedPreferences sp = context.getSharedPreferences(CUSTOM_INFO, Context.MODE_PRIVATE);
+        return sp.getString(name, null);
     }
 
     public void clearConfiguredPkg(Context context, String key) {
-        SharedPreferences sp = context.getSharedPreferences(CUSTOM_INFO,
-                Context.MODE_PRIVATE);
+        SharedPreferences sp = context.getSharedPreferences(CUSTOM_INFO, Context.MODE_PRIVATE);
         Editor spe = sp.edit();
         spe.remove(key);
         spe.commit();
     }
-    
+
     public boolean getConfigured(Context context, String name) {
-        SharedPreferences sp = context.getSharedPreferences(CUSTOM_INFO,
-                Context.MODE_PRIVATE);
+        SharedPreferences sp = context.getSharedPreferences(CUSTOM_INFO, Context.MODE_PRIVATE);
         return sp.getBoolean(name, false);
     }
-    
-    
+
 }
