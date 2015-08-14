effaa0226b2b02d59e17f65c2ece14a508208d3c
Tiger Jiang <jiangzehu@hisense.com>
2015-08-14 18:01:32 +0800

    Launcher : Add database info for launcher

---
+++ LuntechLauncher/src/com/luntech/launcher/db/AppContract.java
@@ -0,0 +1,36 @@
+
+package com.luntech.launcher.db;
+
+public class AppContract {
+    public static class Columns {
+        public static final String _ID = "_id";
+        public static final String Download_id = "download_id";
+        public static final String DOWNLOAD_STATUS = "download_status";
+        public static final String A_NAME = "a_name";
+        public static final String A_PACKAGE = "a_package";
+        public static final String A_REPLACE_PACKAGE = "a_replace_package";
+        public static final String A_HAS_REPLACE = "a_has_replace";
+        public static final String A_ACTIVITY = "a_activity";
+        public static final String A_ICON = "a_icon";
+        public static final String A_URL = "a_url";
+        public static final String A_FILE_NAME = "a_file_name";
+        
+        public static final String M_CODE = "m_code";
+        public static final String M_REPLACE = "m_replace";
+        public static final String M_TYPE = "m_type";
+        public static final String M_TEXT = "m_text";
+        public static final String M_BG = "m_bg";
+        public static final String M_ICON = "m_icon";
+        public static final String M_SHADOW = "m_shadow";
+        
+        public static final String G_CODE = "g_code";
+        public static final String G_MOVEABLE = "g_moveable";
+        public static final String G_FLAG = "g_flag";
+
+        public static final String G_TEXT = "g_text";
+        public static final String G_BG = "g_bg";
+        public static final String G_ICON = "g_icon";
+
+    }
+
+}
--- LuntechLauncher/src/com/luntech/launcher/db/DataOpenHelper.java
+++ LuntechLauncher/src/com/luntech/launcher/db/DataOpenHelper.java
@@ -2,35 +2,40 @@
 package com.luntech.launcher.db;
 
 import android.content.Context;
-import android.database.DatabaseErrorHandler;
 import android.database.sqlite.SQLiteDatabase;
-import android.database.sqlite.SQLiteDatabase.CursorFactory;
 import android.database.sqlite.SQLiteOpenHelper;
 
 public class DataOpenHelper extends SQLiteOpenHelper {
-    private static final int DATABASE_VERSION = 1;
-    private static final String DATABASE_NAME = "app_info.db";
-    private static final String APPLICATION_TABLE_NAME = "app_info";
+    public static final int DATABASE_VERSION = 1;
+    public static final String DATABASE_NAME = "app_info.db";
+    public static final String APPLICATION_TABLE_NAME = "app_info";
     private static final String APPLICATION_TABLE_CREATE = "CREATE TABLE " + APPLICATION_TABLE_NAME
             + " ("
-            + "_id " + " INTEGER primary key autoincrement,"
-            + "download_id " + " INTEGER , "
-            + "a_name " + " varchar, "
-            + "a_package " + " varchar, "
-            + "a_activity " + " varchar, "
-            + "a_icon " + " varchar, "
-            + "a_url " + " varchar, "
-            + "m_code " + " varchar, "
-            + "m_replace " + " varchar, "
-            + "m_type " + " varchar, "
-            + "m_text " + " varchar, "
-            + "m_bg " + " varchar, "
-            + "m_icon " + " varchar, "
-            + "m_shadow " + " varchar, "
-            + "g_code " + " varchar, "
-            + "g_flag " + " varchar, "
-            + "g_text " + " varchar, "
-            + "g_icon " + " varchar)";
+            + "_id" + " integer primary key autoincrement,"
+            + "download_id" + " integer  default 0, "
+            + "download_status" + " varchar, "
+            + "a_name" + " varchar, "
+            + "a_package" + " varchar, "
+            + "a_replace_package" + " varchar, "
+            + "a_has_replace" + " integer  default 0, "
+            + "a_activity" + " varchar, "
+            + "a_icon" + " varchar, "
+            + "a_url" + " varchar, "
+            + "a_file_name" + " varchar, "
+            + "m_code" + " varchar, "
+            + "m_replace" + " varchar, "
+            + "m_type" + " varchar, "
+            + "m_text" + " varchar, "
+            + "m_bg" + " varchar, "
+            + "m_icon" + " varchar, "
+            + "m_shadow" + " varchar, "
+            
+            + "g_code" + " varchar, "
+            + "g_moveable" + " varchar, "
+            + "g_flag" + " varchar, "
+            + "g_text" + " varchar, "
+            + "g_bg" + " varchar, "
+            + "g_icon" + " varchar)";
 
     public DataOpenHelper(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
