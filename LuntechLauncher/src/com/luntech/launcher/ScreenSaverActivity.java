662185a94c76b99c0d18aa0e885a5a67552a9065
Tiger Jiang <tiger.jiang86@gmail.com>
2015-08-16 22:09:35 +0800

    Launcher :  Change the eclipse project to studio project.

--- LuntechLauncher/src/com/luntech/launcher/ScreenSaverActivity.java
+++ LuntechLauncher/src/com/luntech/launcher/ScreenSaverActivity.java
@@ -5,22 +5,23 @@ import java.util.ArrayList;
 import java.util.List;
 
 import android.app.Activity;
+import android.content.Context;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
 
 import com.luntech.launcher.view.AutoScrollViewPager;
 import com.luntech.launcher.view.ImagePagerAdapter;
 
 /**
  * AutoScrollViewPagerDemo
- * 
- * @author <a href="http://www.trinea.cn" target="_blank">Trinea</a> 2014-2-22
  */
 public class ScreenSaverActivity extends Activity {
 
     private AutoScrollViewPager viewPager;
 
-    private List<Drawable> imageIdList;
+    private static List<Drawable> imageIdList;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -30,6 +31,7 @@ public class ScreenSaverActivity extends Activity {
         viewPager = (AutoScrollViewPager) findViewById(R.id.view_pager);
 
         imageIdList = new ArrayList<Drawable>();
+        initScreenSaverRes();
         viewPager.setAdapter(new ImagePagerAdapter(this, imageIdList).setInfiniteLoop(true));
 
         viewPager.setInterval(2000);
@@ -52,4 +54,51 @@ public class ScreenSaverActivity extends Activity {
         // start auto scroll when onResume
         viewPager.startAutoScroll();
     }
+
+    private void initScreenSaverRes() {
+        if (Launcher.sScreenSaverFileList != null && Launcher.sScreenSaverFileList.size() > 0) {
+            for (String filePath : Launcher.sScreenSaverFileList) {
+                Drawable drawable = changeFiletoDrawable(
+                        ScreenSaverActivity.this.getApplicationContext(), filePath);
+                imageIdList.add(drawable);
+            }
+        }
+    }
+
+    public Drawable changeIdtoDrawable(Context context, String name) {
+        Drawable icon = null;
+        int resId = context.getResources()
+                .getIdentifier(name, "drawable", context.getPackageName());
+        if (resId == 0) {
+            Log.e("error", context.getPackageName() + " resource not found for " + name);
+        } else {
+            icon = context.getResources().getDrawable(resId);
+        }
+        return icon;
+    }
+
+    public Drawable changeFiletoDrawable(Context context, String filePath) {
+        Drawable icon = null;
+        Log.d("jzh", "change path " + filePath);
+        if (!TextUtils.isEmpty(filePath)) {
+            icon = Drawable.createFromPath(filePath);
+        }
+        return icon;
+    }
+
+    public Drawable getDrawableFromAttribute(Context context, String attribute) {
+        if (attribute.endsWith(".png") | attribute.endsWith(".PNG") | attribute.endsWith(".JPG")
+                | attribute.endsWith(".jpg")) {
+            return changeFiletoDrawable(context, attribute);
+        } else {
+            return changeIdtoDrawable(context, attribute);
+        }
+    }
+    
+    @Override
+    public void onUserInteraction() {
+        super.onUserInteraction();
+        finish();
+    }
+
 }
