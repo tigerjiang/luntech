1790822573a8eeec70ea5851c0ca9807c04a8548
Tiger Jiang <jiangzehu@hisense.com>
2015-09-23 16:28:50 +0800

    Launcher : Fix the bugs

--- LuntechLauncher/src/com/luntech/launcher/secondary/ApplicationInfo.java
+++ LuntechLauncher/src/com/luntech/launcher/secondary/ApplicationInfo.java
@@ -90,7 +90,7 @@ public class ApplicationInfo implements Parcelable, Comparable<ApplicationInfo> 
         String packageName = mComponent.getPackageName();
         String activityName = mComponent.getClassName();
         if(("com.skzh.elifetv.MainActivity").equals(activityName)){
-            intent.putExtra("frag_index", "1");
+            i.putExtra("frag_index", "1");
         }
         Log.d("AppCenter", "packageName is " + packageName);
         i.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
